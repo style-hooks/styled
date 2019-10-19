878a0023bc42e60fd5676056275909499c06ae67
Jared Lunde <jared.lunde@gmail.com>
2019-10-18 19:43:39 -0600

    house cleaning

--- test/test-utils.js
+++ test/test-utils.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import {ThemeProvider} from '@style-hooks/core'
 
-
 export const renderWithTheme = (children, theme = {}, options = {}) =>
   render(children, {
     wrapper: ({children}) => (
